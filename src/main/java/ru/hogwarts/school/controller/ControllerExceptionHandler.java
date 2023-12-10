7afb6e267a75e913a07b1dfe61f9da4e1f1d5e30
Kseniia313 <kshome@mail.ru>
2023-12-10 13:29:48 +0300

    ДЗ Логирование и крнфигурирование

--- src/main/java/ru/hogwarts/school/controller/ControllerExceptionHandler.java
+++ src/main/java/ru/hogwarts/school/controller/ControllerExceptionHandler.java
@@ -1,15 +1,21 @@
 package ru.hogwarts.school.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import ru.hogwarts.school.exception.*;
+import ru.hogwarts.school.service.StudentServiceImpl;
 
 @ControllerAdvice
 public class ControllerExceptionHandler {
+    private long id;
+    Logger logger = LoggerFactory.getLogger(StudentServiceImpl.class);
     @ExceptionHandler({StudentAlreadyExistException.class, FacultyAlreadyExistException.class})
     public ResponseEntity<String> handleException(RuntimeException exception) {
+
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
                 .body(exception.getMessage());
--- src/main/java/ru/hogwarts/school/service/FacultyServiceImpl.java
+++ src/main/java/ru/hogwarts/school/service/FacultyServiceImpl.java
@@ -1,5 +1,7 @@
 package ru.hogwarts.school.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import ru.hogwarts.school.exception.FacultyNotFoundException;
 import ru.hogwarts.school.model.Faculty;
@@ -10,6 +12,7 @@ import java.util.*;
 
 @Service
 public class FacultyServiceImpl implements FacultyService {
+    Logger logger = LoggerFactory.getLogger(FacultyServiceImpl.class);
 
     private final FacultyRepository facultiesRepository;
 
@@ -19,12 +22,14 @@ public class FacultyServiceImpl implements FacultyService {
 
     @Override
     public Faculty createFaculty(Faculty faculty) {
+        logger.info("Was invoked method for creating faculty");
         return facultiesRepository.save(faculty);
     }
 
     @Override
     public Faculty getFacultyById(long facultyId) {
-
+if(!facultiesRepository.existsById(facultyId))
+    logger.error("Факультет c id {} не найден", facultyId);
         return facultiesRepository.findById(facultyId)
                 .orElseThrow(() -> new FacultyNotFoundException("Факультет не найден"));
 
@@ -32,14 +37,14 @@ public class FacultyServiceImpl implements FacultyService {
 
     @Override
     public Faculty upDateFaculty(Faculty faculty) {
+        logger.info("Was invoked method for update faculty c id {}",faculty.getId());
         getFacultyById(faculty.getId());
-
         return facultiesRepository.save(faculty);
     }
 
     @Override
     public Faculty deleteFaculty(long facultyId) {
-
+        logger.info("Was invoked method for remove faculty c id {}",facultyId);
         Faculty faculty = getFacultyById(facultyId);
         facultiesRepository.delete(faculty);
         return faculty;
@@ -47,11 +52,13 @@ public class FacultyServiceImpl implements FacultyService {
 
     @Override
     public Collection<Faculty> findByColor(String color) {
+        logger.info("Was invoked method for finding faculty by color {}",color);
         return facultiesRepository.findAllByColor(color);
     }
 
     @Override
     public Collection<Faculty> findByNameOrColor(String name, String color) {
+        logger.info("Was invoked method for finding faculties by name {} or color {} ",name,color);
         return facultiesRepository.findAllByNameIgnoreCaseOrColorIgnoreCase(name, color);
     }
 
